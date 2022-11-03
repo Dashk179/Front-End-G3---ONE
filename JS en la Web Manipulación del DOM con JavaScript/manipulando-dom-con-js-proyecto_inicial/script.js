8fcc1f749dfe0b62d53d16dbc4617328bd48854b
Arturo Perez <dash_arturo@hotmail.com>
2022-11-02 19:45:42 -0600

    Agregamos la clase CSS utilizand toggle, implementacion del atributo parentElement para subir un elemento en el arbol del DOM, encontrar el objetivo del evento utilizando la propiedad target,Se agrego Expresion de funcion ejecutada inmediatamente

--- JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/script.js
+++ JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,27 +1,49 @@
-const btn = document.querySelector("[data-form-btn]");
+(()  => {
+    const btn = document.querySelector("[data-form-btn]");
 
 //Arrow function o funciones anonimas
 const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[ data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]")
-    const task =  document.createElement('li');
-    task.classList.add("card")
-    input.value =""
-    //Backticks
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
+  evento.preventDefault();
+  const input = document.querySelector("[ data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+
+  //Backticks
+  const taskContent = document.createElement("div");
+  taskContent.appendChild(checkComplete());
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(titleTask);
+  const content = `
+   
   </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
-  task.innerHTML = content;
-  list.appendChild(task)
-    console.log(content);
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  // task.innerHTML = content;
+  task.appendChild(taskContent);
+  list.appendChild(task);
+  console.log(content);
 };
 
 console.log(btn);
 
+btn.addEventListener("click", createTask);
 
-btn.addEventListener('click', createTask);
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far","fa-check-square","icon");
+  i.addEventListener("click",completeTask);
+  return i;
+};
 
+//Immediately invoked function expresion IIFE
+ const completeTask = (evento) => {
+    const element = evento.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+};
+})()
