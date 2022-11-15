809a0a8e5724b7ad442fb7f1629ff5f2bd8955c1
Arturo Perez <dash_arturo@hotmail.com>
2022-11-14 20:45:47 -0600

    Aplicando polimorfismo en nuetro Sistema de autentificacion

--- JavaScript_Herencia_e_Interfaces_en_Orientación_a_Objetos/JS-herencia-polimorfismo-Proyecto/index.js
+++ JavaScript_Herencia_e_Interfaces_en_Orientación_a_Objetos/JS-herencia-polimorfismo-Proyecto/index.js
@@ -1,23 +1,23 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import { Cuenta } from './Cuentas/Cuenta.js';
-import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
-import {CuentaCorriente} from './Cuentas/CuentaCorriente.js';
-import { CuentaNomina } from './Cuentas/CuentaNomina.js';
 import { Director } from './Empleados/Director.js';
 import { Empleado } from './Empleados/empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
+cliente.asignarClave('1111');
+console.log(SistemaAutenticacion.login(cliente,'1111'));
+//const cliente2 = new Cliente('María','16979808','8989');
 
 const empleado = new Empleado('Juan Perez','1231212',10000);
- const gerente = new Gerente('Arturo Perez','23333',12000);
- const director = new Director('Josue Perez','44444',15000);
+empleado.asignarClave('12345');
+const gerente = new Gerente('Arturo Perez','23333',12000);
+const director = new Director('Josue Perez','44444',15000);
 
 console.log(empleado.verBonificacion());
  console.log(gerente.verBonificacion());
  console.log(director.verBonificacion());
 
 
-
+console.log(SistemaAutenticacion.login(empleado,'12345'));
