6be08c914bfa6517c3a291ef2d4eb30b25ecc54b
Arturo Perez <dash_arturo@hotmail.com>
2022-11-17 11:51:12 -0600

    Utilizar el componente de calendario, manipular flechas

--- JavaScript-el-lenguaje-de_la-web/JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/script.js
+++ JavaScript-el-lenguaje-de_la-web/JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/script.js
@@ -2,12 +2,23 @@ import checkComplete from "./components/checkComplete.js";
 import deleteIcon from "./components/deleteIcon.js";
 const btn = document.querySelector("[data-form-btn]");
 
+
+const addTask = (evento) =>{
+  const list = document.querySelector("[data-list]");
+  const task = createTask(evento);
+  list.appendChild(task);
+}
+
 //Arrow function o funciones anonimas
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector("[ data-form-input]");
   const value = input.value;
-  const list = document.querySelector("[data-list]");
+  const calendar = document.querySelector("[data-form-date]");
+  const date = calendar.value;
+  const dataFormat = moment(date).format('DD/MM/YYYY');
+  console.log(dataFormat);
+  
   const task = document.createElement("li");
   task.classList.add("card");
   input.value = "";
@@ -24,15 +35,19 @@ const createTask = (evento) => {
   </div>
   <i class="fas fa-trash-alt trashIcon icon"></i>`;
   // task.innerHTML = content;
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dataFormat;
+
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
-  console.log(content);
+  
+  return task;
 };
 
 console.log(btn);
 
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
 
 
 
--- JavaScript_Herencia_e_Interfaces_en_Orientación_a_Objetos/JS-herencia-polimorfismo-Proyecto/index.js
+++ JavaScript_Herencia_e_Interfaces_en_Orientación_a_Objetos/JS-herencia-polimorfismo-Proyecto/index.js
@@ -5,29 +5,16 @@ import { Empleado } from './Empleados/empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
 import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-cliente.asignarClave('1111');
-//console.log(SistemaAutenticacion.login(cliente,'1111'));
-//const cliente2 = new Cliente('María','16979808','8989');
-
-const empleado = new Empleado('Juan Perez','1231212',10000);
+const empleado = new Empleado('Juan Perez','1234343',10000);
 empleado.asignarClave('12345');
-
-
-const gerente = new Gerente('Arturo Perez','23333',12000);
-gerente.asignarClave('54321');
-
- const director = new Director('Josue Perez','44444',15000);
-
-
-
-console.log(empleado.verBonificacion());
- console.log(gerente.verBonificacion());
- console.log(director.verBonificacion());
-
-
 console.log(SistemaAutenticacion.login(empleado,'12345'));
 
-console.log(SistemaAutenticacion.login(gerente,'54321'));
+const gerente = new Gerente('Pedro Rivas','232344',12000);
+gerente.asignarClave('655');
+console.log(SistemaAutenticacion.login(gerente,'655'));
 
-console.log(SistemaAutenticacion.login(cliente,'1111'));
+//const director = new Director('Elena Moreno','232323',15000);
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+cliente.asignarClave('01111');
+console.log(SistemaAutenticacion.login(cliente,'01111'));
