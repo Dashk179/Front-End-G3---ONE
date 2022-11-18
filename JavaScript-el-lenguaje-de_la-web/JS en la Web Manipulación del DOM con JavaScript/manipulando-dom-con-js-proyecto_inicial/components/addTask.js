7337ffca00dfcd592db25f4dadd7b59633022ddd
Arturo Perez <dash_arturo@hotmail.com>
2022-11-17 22:20:07 -0600

    Recorrer el arreglo con forEach de las tareas guardadas en LocalStorage, utilizar pipe y Convertir las tareas almacenadas en formato string a objetos JSON.parse()

--- JavaScript-el-lenguaje-de_la-web/JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/components/addTask.js
+++ JavaScript-el-lenguaje-de_la-web/JS en la Web Manipulación del DOM con JavaScript/manipulando-dom-con-js-proyecto_inicial/components/addTask.js
@@ -1,59 +1,56 @@
 import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
-export const addTask = (evento) =>{
-    const list = document.querySelector("[data-list]");
-    const task = createTask(evento);
-    list.appendChild(task);
-  }
+export const addTask = (evento) => {
+  evento.preventDefault();
 
+  const list = document.querySelector("[data-list]");
+  const input = document.querySelector("[ data-form-input]");
+  const calendar = document.querySelector("[data-form-date]");
 
-  
-  //Arrow function o funciones anonimas
-  const createTask = (evento) => {
-     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("task")) || [];//En caso de que sea null se le da un valor por defecto
-    console.log(taskList);
-    const input = document.querySelector("[ data-form-input]");
-    const value = input.value;
-    const calendar = document.querySelector("[data-form-date]");
-    const date = calendar.value;
-    const dateFormat = moment(date).format('DD/MM/YYYY');
-    console.log(dateFormat);
-    
-    const task = document.createElement("li");
+  const value = input.value;
+  const date = calendar.value;
+  const dateFormat = moment(date).format("DD/MM/YYYY");
+
+  input.value = "";
+  calendar.value = "";
+
+  const taskObjt = {
+    value,
+    dateFormat,
+  };
+
+  const taskList = JSON.parse(localStorage.getItem("task")) || []; //En caso de que sea null se le da un valor por defecto
+  taskList.push(taskObjt);
+
+  localStorage.setItem("task", JSON.stringify(taskList));
+
+  const task = createTask(taskObjt);
+  list.appendChild(task);
+};
+
+//Arrow function o funciones anonimas
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement("li");
     task.classList.add("card");
-    input.value = "";
-  
-    //Backticks
-    const taskContent = document.createElement("div");
+
+  //Backticks
+  const taskContent = document.createElement("div");
+
+ 
+  const titleTask = document.createElement("span");
+   titleTask.classList.add("task");
+   titleTask.innerText = value;
+   taskContent.appendChild(checkComplete());
+   taskContent.appendChild(titleTask);
+
+  // task.innerHTML = content;
+  const dateElement = document.createElement("span");
     
-    const taskObjt = {
-      value,
-      dateFormat
-    }
-  
-    taskList.push(taskObjt);
-  
-    localStorage.setItem("task",JSON.stringify(taskList));
-  
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(titleTask);
-    const content = `
-     
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    // task.innerHTML = content;
-    const dateElement = document.createElement("span");
     dateElement.innerHTML = dateFormat;
-  
     task.appendChild(taskContent);
     task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    
-    return task;
-  };
-  
+
+  return task;
+};
