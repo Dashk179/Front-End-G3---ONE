68a9fef53f3572799aa3811b13af4af266c21d5b
Arturo Perez <dash_arturo@hotmail.com>
2022-11-21 20:48:49 -0600

    Utilizar async/await para indicar funciones asincronas, tratar los errores con try/catch

--- CRUD_JS_Async/controller/actualizar.controller.js
+++ CRUD_JS_Async/controller/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacon = () =>{
+const obtenerInformacon = async() =>{
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
 
@@ -12,14 +12,23 @@ const obtenerInformacon = () =>{
     
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
-    console.log(nombre ,"-",email);
-    
-    clientServices.detalleCliente(id).then(perfil => 
-       {
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-       })
+
+    try{
+        const perfil = await clientServices.detalleCliente(id)
+        console.log(perfil);
+        if(perfil.nombre && perfil.email){
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        }
+        else{
+            throw new Error();
+        }
+        }catch(error){
+        window.location.href = "/screens/error.html"
     }
+
+   
+      }
 
 obtenerInformacon();
 
